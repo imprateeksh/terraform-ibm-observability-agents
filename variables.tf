5fba45d71bab640f6c489e80a74795fee140043e
prateek <sharma_prateek@outlook.com>
2025-04-17 12:14:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,12 +7,12 @@ variable "cluster_id" {
   description = "The ID of the cluster you wish to deploy the agents in"
 }
 
-variable "cluster_resource_group_id" {
+variable "resource_group_id" {
   type        = string
   description = "The Resource Group ID of the cluster"
 }
 
-variable "cluster_config_endpoint_type" {
+variable "config_endpoint_type" {
   description = "Specify which type of endpoint to use for for cluster config access: 'default', 'private', 'vpe', 'link'. 'default' value will use the default endpoint of the cluster."
   type        = string
   default     = "default"
@@ -51,6 +51,20 @@ variable "wait_till_timeout" {
   default     = 90
 }
 
+variable "logs_agent_tolerations" {
+  description = "List of tolerations to apply to Logs agent. The default value means a pod will run on every node."
+  type = list(object({
+    key               = optional(string)
+    operator          = optional(string)
+    value             = optional(string)
+    effect            = optional(string)
+    tolerationSeconds = optional(number)
+  }))
+  default = [{
+    operator = "Exists"
+  }]
+}
+
 ##############################################################################
 # Cloud Monitoring variables
 ##############################################################################
@@ -74,13 +88,6 @@ variable "cloud_monitoring_access_key" {
   description = "Access key used by the IBM Cloud Monitoring agent to communicate with the instance"
   sensitive   = true
   default     = null
-}
-
-variable "cloud_monitoring_secret_name" {
-  type        = string
-  description = "The name of the secret which will store the access key."
-  default     = "sysdig-agent"
-  nullable    = false
 }
 
 variable "cloud_monitoring_instance_region" {
@@ -144,12 +151,6 @@ variable "cloud_monitoring_add_cluster_name" {
   default     = true
 }
 
-variable "cloud_monitoring_agent_name" {
-  description = "Cloud Monitoring agent name. Used for naming all kubernetes and helm resources on the cluster."
-  type        = string
-  default     = "sysdig-agent"
-}
-
 variable "cloud_monitoring_agent_namespace" {
   type        = string
   description = "Namespace where to deploy the Cloud Monitoring agent. Default value is 'ibm-observe'"
@@ -180,6 +181,13 @@ variable "cloud_monitoring_agent_tolerations" {
 # Logs Agents variables
 ##############################################################################
 
+variable "cloud_monitoring_secret_name" {
+  type        = string
+  description = "The name of the secret which will store the access key."
+  default     = "sysdig-agent"
+  nullable    = false
+}
+
 variable "logs_agent_enabled" {
   type        = bool
   description = "Whether to deploy the Logs agent."
@@ -200,21 +208,6 @@ variable "logs_agent_namespace" {
   nullable    = false
 }
 
-variable "logs_agent_trusted_profile" {
-  type        = string
-  description = "The IBM Cloud trusted profile ID. Used only when `logs_agent_iam_mode` is set to `TrustedProfile`. The trusted profile must have an IBM Cloud Logs `Sender` role."
-  default     = null
-
-  validation {
-    condition = (
-      var.logs_agent_enabled == false ||
-      var.logs_agent_iam_mode != "TrustedProfile" ||
-      (var.logs_agent_trusted_profile != null && var.logs_agent_trusted_profile != "")
-    )
-    error_message = "When passing 'TrustedProfile' value for 'logs_agent_iam_mode' you cannot set 'logs_agent_trusted_profile' as null or empty string."
-  }
-}
-
 variable "logs_agent_iam_api_key" {
   type        = string
   description = "The IBM Cloud API key for the Logs agent to authenticate and communicate with the IBM Cloud Logs. It is required if `logs_agent_iam_mode` is set to `IAMAPIKey`."
@@ -231,18 +224,10 @@ variable "logs_agent_iam_api_key" {
   }
 }
 
-variable "logs_agent_tolerations" {
-  description = "List of tolerations to apply to Logs agent. The default value means a pod will run on every node."
-  type = list(object({
-    key               = optional(string)
-    operator          = optional(string)
-    value             = optional(string)
-    effect            = optional(string)
-    tolerationSeconds = optional(number)
-  }))
-  default = [{
-    operator = "Exists"
-  }]
+variable "cloud_monitoring_agent_name" {
+  description = "Cloud Monitoring agent name. Used for naming all kubernetes and helm resources on the cluster."
+  type        = string
+  default     = "sysdig-agent"
 }
 
 variable "logs_agent_additional_log_source_paths" {
@@ -273,7 +258,7 @@ variable "logs_agent_log_source_namespaces" {
   nullable    = false
 }
 
-variable "logs_agent_iam_mode" {
+variable "agent_iam_mode" {
   type        = string
   default     = "TrustedProfile"
   description = "IAM authentication mode: `TrustedProfile` or `IAMAPIKey`."
@@ -318,4 +303,19 @@ variable "cloud_logs_ingress_port" {
   type        = number
   default     = 3443
   description = "The target port for the IBM Cloud Logs ingestion endpoint. The port must be 443 if you connect by using a VPE gateway, or port 3443 when you connect by using CSEs."
+}
+
+variable "logs_agent_trusted_profile" {
+  type        = string
+  description = "The IBM Cloud trusted profile ID. Used only when `logs_agent_iam_mode` is set to `TrustedProfile`. The trusted profile must have an IBM Cloud Logs `Sender` role."
+  default     = null
+
+  validation {
+    condition = (
+      var.logs_agent_enabled == false ||
+      var.logs_agent_iam_mode != "TrustedProfile" ||
+      (var.logs_agent_trusted_profile != null && var.logs_agent_trusted_profile != "")
+    )
+    error_message = "When passing 'TrustedProfile' value for 'logs_agent_iam_mode' you cannot set 'logs_agent_trusted_profile' as null or empty string."
+  }
 }
